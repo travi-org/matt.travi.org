889185194c893487ec552b8c924ad7c101b2da77
Matt Travi <programmer@travi.org>
2019-09-14 23:56:09 -0500

    test(primary-nav): added a simple cypress test
    to ensure the drawer opens

--- test/e2e/smoke-test.js
+++ test/e2e/smoke-test.js
@@ -4,4 +4,12 @@ describe('Smoke Test', () => {
 
     cy.get('main h1').should('have.text', 'Matt Travi');
   });
+
+  it('should provide primary navigation', () => {
+    cy.visit('/');
+
+    cy.get('header button').click();
+
+    cy.get('div.MuiDrawer-root').should('be.visible');
+  });
 });
