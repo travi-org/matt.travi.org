8da3a6fe7df3fc2476920ca0e101823ace8a216c
Matt Travi <programmer@travi.org>
2019-12-31 20:19:09 -0600

    feat(contact): added a route for the contact-form

---
+++ src/pages/contact.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import {Contact} from '@travi/matt.travi.org-components';
+import SiteLayout from '../components/layout';
+
+export default function () {
+  return (
+    <SiteLayout>
+      <Contact />
+    </SiteLayout>
+  );
+}
