428b27b515152e30f0144e9b8f2074afa7f2cab9
Matt Travi <programmer@travi.org>
2021-09-09 23:06:52 -0500

    test(lint): defined names for the page components

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -2,7 +2,7 @@ import React from 'react';
 import {Contact} from '@travi/matt.travi.org-components';
 import SiteLayout from '../components/layout';
 
-export default function () {
+export default function WrappedContact() {
   return (
     <SiteLayout>
       <Contact name="contact" action="/contact-results" />
